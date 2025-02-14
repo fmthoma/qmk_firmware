3490927829849ef41f2de3f3600d949a19c9052f
Franz Thoma <franz.thoma@tngtech.com>
2025-02-14 14:18:05 +0100

    Start poor mans neo

--- keyboards/input_club/ergodox_infinity/keymaps/fmthoma/keymap.c
+++ keyboards/input_club/ergodox_infinity/keymaps/fmthoma/keymap.c
@@ -5,6 +5,9 @@ enum custom_layers {
     BASE,   // default layer
     ARRW,   // Arrow keys
     NUM,    // Num pad
+
+    NEO1,   // Poor man's Neo layer 1
+
     SYMB,   // symbols
     MDIA,   // media keys
 };
@@ -80,7 +83,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// Arrow keys
 [ARRW] = LAYOUT_ergodox(
        // left hand
        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_TRNS,
@@ -122,7 +124,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-// Num pad
 [NUM] = LAYOUT_ergodox(
        // left hand
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -143,6 +144,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_P0
 ),
+/* Keymap 3: Poor man's Neo, layer 1. Based on German layout.
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   ^    |   1  |   2  |   3  |   4  |   5  | Ins  |           | Esc  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Cut    |   X  |   V  |   L  |   C  |   W  | Del  |           | Bksp |   K  |   H  |   G  |   F  |   Q  |   ẞ    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Copy   |   U  |   I  |   A  |   E  |   O  |------|           |------|   S  |   N  |   R  |   T  |   D  |   Y    |
+ * |--------+------+------+------+------+------| Tab  |           | Enter|------+------+------+------+------+--------|
+ * | Paste  |   Ü  |   Ö  |   Ä  |   P  |   Z  |      |           |      |   B  |   M  |   ,  |   .  |   J  |  NUM   |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | ???  | ???  | ???  | ???  | GUI  |                                       | ARRW |  F4  |  ??? |  ??? |C+S+F12|
+ *   `----------------------------------'                                       `-----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | ???  | ???  |       | ???  | ???  |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | LAlt |       | ???  |      |      |
+ *                                 | Shift| Caps |------|       |------| AltGr| Space|
+ *                                 |      |      | LCtrl|       | RCtrl|      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[NEO1] = LAYOUT_ergodox(  // layer 0 : default
+        // left hand
+        DE_CIRC,        KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_INS,
+        LCTL(KC_X),     KC_X,         KC_V,   KC_L,   KC_C,   KC_W,   KC_DEL,
+        LCTL(KC_C),     KC_U,         KC_I,   KC_A,   KC_E,   KC_O,
+        LCTL(KC_V),     DE_UDIA,      DE_ODIA,DE_ADIA,KC_P,   KC_Z,   KC_TAB,
+        KC_NO,          KC_NO,        KC_NO,  KC_LGUI,
+                                                      KC_NO,  KC_NO,
+                                                              KC_LALT,
+                                              KC_LSFT,KC_CAPS,KC_LCTL,
+        // right hand
+             KC_ESC,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             DE_MINS,
+             KC_BSPC,     KC_K,   KC_H,  KC_G,   KC_F,   KC_Q,             DE_SS,
+                          KC_S,   KC_N,  KC_R,   KC_T,   KC_D,             DE_Y,
+             KC_ENT,      KC_B,   KC_M,  KC_COMM,KC_DOT, KC_J,             TG(2),  
+                                  MO(1), KC_F4,  KC_NO,  KC_NO,            MEH(KC_12),
+             KC_NO,       KC_NO,
+             KC_NO,
+             KC_RCTRL,KC_RALT,KC_SPC
+    ),
 /* Keymap 1: Symbol Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
