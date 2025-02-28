96ee63f91ae3c934136227627c8e50ef0d1108cf
Franz Thoma <franz.thoma@tngtech.com>
2025-02-28 11:20:43 +0100

    Layer indicator (copy-paste from qmk docs)

--- keyboards/crkbd/keymaps/fmthoma/config.h
+++ keyboards/crkbd/keymaps/fmthoma/config.h
@@ -25,6 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define QUICK_TAP_TERM 0
 //#define TAPPING_TERM 100
 
+#define SPLIT_LAYER_STATE_ENABLE
+
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
