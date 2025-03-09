77cb542d4c76f7e9090daef9541b0296a27cd587
Franz Thoma <franz.thoma@tngtech.com>
2025-03-09 20:44:46 +0100

    Fix some keys I forgot to implement/change

--- keyboards/crkbd/keymaps/fmthoma/keymap.c
+++ keyboards/crkbd/keymaps/fmthoma/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------------------------------------------------------.    ,-------------------------------------------------------------.
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,     _______, XXXXXXX,ZOOM_OUT, ZOOM_IN, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+-------|    |-------+--------+--------+--------+--------+--------+--------|
-      _______, KC_LGUI, KC_LSFT, KC_LALT, KC_LCTL, XXXXXXX,_______,     _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
+      _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,_______,     _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+-------'    `-------+--------+--------+--------+--------+--------+--------|
       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -122,9 +122,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
     [NUMFN] = LAYOUT_split_3x6_3_ex2(
   //,-------------------------------------------------------------.    ,-------------------------------------------------------------.
-      XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,_______,     _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,MINS_GRV,
+      DE_CIRC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,_______,     _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,MINS_GRV,
   //|--------+--------+--------+--------+--------+--------+-------|    |-------+--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LGUI, KC_LSFT, KC_LALT, KC_LCTL, XXXXXXX,_______,     _______, XXXXXXX, KC_RCTL, KC_LALT, KC_RSFT, KC_RGUI, XXXXXXX,
+      XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,_______,     _______, XXXXXXX, KC_RCTL, KC_RSFT, KC_LALT, KC_RGUI, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+-------'    `-------+--------+--------+--------+--------+--------+--------|
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-------------------------------------------------------------.    ,-------------------------------------------------------------.
      TG(NEO1), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,     _______,  KC_NUM,   KC_P7,   KC_P8,   KC_P9, KC_PMNS, KC_PSLS,
   //|--------+--------+--------+--------+--------+--------+-------|    |-------+--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_LGUI, KC_LSFT, KC_LALT, KC_LCTL, XXXXXXX,_______,     _______, XXXXXXX,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PAST,
+      XXXXXXX, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, XXXXXXX,_______,     _______, XXXXXXX,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PAST,
   //|--------+--------+--------+--------+--------+--------+-------'    `-------+--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,   KC_P1,   KC_P2,   KC_P3, KC_PENT, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
